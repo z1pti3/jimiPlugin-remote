e673b495dd6a7094e0bf0d5571591baeee591e22
Patrick Lynch <patrick.lynch@unipart.com>
2020-10-23 16:28:40 +0100

    try and except handling for SMB session register

--- includes/windows.py
+++ includes/windows.py
@@ -74,7 +74,11 @@ class windows():
             return self.executeCommand(command,args,elevate)
 
     def upload(self,localFile,remotePath):
-        smbclient.register_session(self.host, username=self.username, password=self.password, connection_timeout=30)
+        try:
+            smbclient.register_session(self.host, username=self.username, password=self.password, connection_timeout=30)
+        except Exception as e:
+            self.error = str(e)
+            return False
         # single file
         if not os.path.isdir(localFile):
             try:
@@ -127,7 +131,11 @@ class windows():
         return False
 
     def download(self,remoteFile,localPath):
-        smbclient.register_session(self.host, username=self.username, password=self.password,connection_timeout=30)
+        try:
+            smbclient.register_session(self.host, username=self.username, password=self.password,connection_timeout=30)
+        except Exception as e:
+            self.error = str(e)
+            return False
         try:
             with open(localPath, mode="wb") as f:
                 with smbclient.open_file("\\{0}\{1}".format(self.host,remoteFile), mode="rb") as remoteFile:
